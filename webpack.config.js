93692c97c2dddc24780bcd8d39c6d00f7099608c
xzw <1272117264@qq.com>
2020-10-12 22:48:51 +0800

    项目初始化

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module:{
+    rules: [
+      {
+        test: /\.less$/,
+        use:[
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use:[
+          'file-loader',
+        ]
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        options: {
+          attributes: {
+            list: [
+              {
+                tag: 'img',
+                attribute: 'src',
+                type: 'src',
+              },
+              {
+                tag: 'link',
+                attribute: 'href',
+                type: 'src',
+              }
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
