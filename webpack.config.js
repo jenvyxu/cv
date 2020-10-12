bac09597d8b79afbc69d8133f991d56ac50386ea
xzw <1272117264@qq.com>
2020-10-12 22:59:12 +0800

    修改webpack打包输出文件位置

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     app: './src/index.js'
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: '[name].bundle.js'
   },
   devServer: {
