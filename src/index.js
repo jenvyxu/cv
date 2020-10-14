9635e02f367ada97be8fd992fc5bff8641d320dc
xzw <1272117264@qq.com>
2020-10-14 08:35:30 +0800

    引入iconfont图标

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
+
 import './style.less'
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-// import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
+import './asset/icon/iconfont.css'
