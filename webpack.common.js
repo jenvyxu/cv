f0085636870b7c7a99657a32e1e87c26f5b0fe4a
xzw <1272117264@qq.com>
2020-10-14 08:37:00 +0800

    分开配置webapck生产环境和开发环境

---
+++ webpack.common.js
@@ -0,0 +1,84 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'docs')
+  },
+  module:{
+    rules: [
+      {
+        test: /\.less$/,
+        use:[
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use:[
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            outputPath: 'fonts'
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use:[
+          'file-loader',
+        ]
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        options: {
+          attributes: {
+            list: [
+              {
+                tag: 'img',
+                attribute: 'src',
+                type: 'src',
+              },
+              {
+                tag: 'link',
+                attribute: 'href',
+                type: 'src',
+              }
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins:[
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+  },
+}
